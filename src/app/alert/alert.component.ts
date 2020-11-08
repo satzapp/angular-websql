50101977dc9ee660f84295749b57b2ccc57ab9c5
satzapp <satzapplications@gmail.com>
2020-11-08 22:12:00 +0530

    NgAlert implementation using ng-bootstrap

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-alert",
   templateUrl: "./alert.component.html"
 })
-export class AlertComponent implements OnInit {
-  dismissible: boolean = true;
-  ngOnInit() {}
-}
+export class AlertComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
@@ -10,7 +11,13 @@ import { StudentService } from "./services/student.service";
 import { AlertComponent } from "./alert/alert.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgbModule
+  ],
   declarations: [
     AppComponent,
     HelloComponent,
